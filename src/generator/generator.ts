58812cccac1faf48dc06cebe8babba8b6f491092
Jess Renteria <jessrenteria@users.noreply.github.com>
2026-04-26 16:36:49 -0700

    Add puzzle generator library.

---
+++ src/generator/generator.ts
@@ -0,0 +1,79 @@
+import { type Puzzle } from '@/model';
+
+export function genPuzzle(rows: number, columns: number): Puzzle {
+  const solution = genSolution(rows, columns);
+
+  return {
+    solution: solution,
+    numRows: rows,
+    numColumns: columns,
+    rowConstraints: getRowConstraints(solution),
+    columnConstraints: getColumnConstraints(solution),
+  };
+}
+
+// Returns a uniform distribution of {true, false}.
+function coinFlip() {
+  return Math.random() >= 0.5;
+}
+
+function genSolution(numRows: number, numColumns: number): boolean[][] {
+  let rows =[];
+  for (let r = 0; r < numRows; ++r) {
+    let row = [];
+    for (let c = 0; c < numColumns; ++c) {
+      row.push(coinFlip());
+    }
+    rows.push(row);
+  }
+  return rows;
+}
+
+function getConstraints(line: boolean[]): number[] {
+  let constraints = [];
+
+  let runLength = 0;
+  for (let i = 0; i < line.length; ++i) {
+    if (line[i]) {
+      ++runLength;
+      continue;
+    }
+
+    if (!line[i] && runLength > 0) {
+      constraints.push(runLength);
+      runLength = 0;
+    }
+  }
+  if (runLength > 0) constraints.push(runLength);
+  if (constraints.length === 0) constraints.push(0);
+
+  return constraints;
+}
+
+function getRowConstraints(solution: boolean[][]): number[][] {
+  return solution.map(getConstraints);
+}
+
+// Transposes a dense m x n matrix into an n x m matrix.
+function transposeMatrix<T>(matrix: T[][]): T[][] {
+  if (matrix.length === 0) return [];
+  if (matrix[0]!.length === 0) return [];
+
+  const numColumns = matrix.length;
+  const numRows = matrix[0]!.length;
+  let rows = [];
+  for (let r = 0; r < numRows; ++r) {
+    let row = [];
+    for (let c = 0; c < numColumns; ++c) {
+      row.push(matrix[c]![r]!);
+    }
+    rows.push(row);
+  }
+  return rows;
+}
+
+function getColumnConstraints(solution: boolean[][]): number[][] {
+  // TODO: Consider avoiding the transpose while minimizing code duplication
+  // by using generators instead of arrays.
+  return transposeMatrix(solution).map(getConstraints);
+}
